4946e11841e537056b7ab935e44ee9f5526f4fbc
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2025-08-03 22:47:57 +0200

    i18n (4/6)

--- web-ui/src/Content/PCUX/Components/Resume.jsx
+++ web-ui/src/Content/PCUX/Components/Resume.jsx
@@ -1,115 +1,118 @@
 import { Tabs, Tab, Button, Fieldset, Frame, Avatar, Checkbox, ProgressBar } from '@react95/core';
 
+import { useTranslation, Trans } from 'react-i18next';
+
 export function Resume() {
+  const { t } = useTranslation();
+
   return (
     <div className="text-left flex flex-col">
-      <Tabs width="350px" defaultActiveTab="About">
-        <Tab title="About">
+      <Tabs width="350px" defaultActiveTab={t('resume.tabs.about')}>
+        <Tab title={t('resume.tabs.about')}>
           <div className="flex flex-col gap-3">
             <p>
-              <b>Hi, I'm Moi</b>
+              <Trans i18nKey="resume.greeting" components={[<b key="0" />]} />
             </p>
             <Avatar alt="Moet logo" size="115px" src="icons/moet.webp" circle />
-            <Fieldset legend="a Software Engineer" width="100%">
+            <Fieldset legend={t('resume.title')} width="100%">
               <Frame display="flex" flexDirection="column" padding="$10">
-                passionate about accessibility, user interaction, and creating inclusive, intuitive
-                digital experiences across all devices. Committed to innovation and continuous
-                improvement.
+                {t('resume.about')}
               </Frame>
             </Fieldset>
           </div>
         </Tab>
-        <Tab title="Education">
+
+        <Tab title={t('resume.tabs.education')}>
           <Fieldset legend="" width="100%">
             <Frame padding="$10">
               <Checkbox readOnly checked>
-                <b>Bachelor in Software Engineering</b> at University of Oviedo (2020)
+                <Trans i18nKey="resume.education.bachelor" components={[<b key="0" />]} />
               </Checkbox>
               <Checkbox readOnly checked>
-                <b>Master’s in Computer Graphics, Virtual Reality & Simulation</b> at U-TAD (2021)
+                <Trans i18nKey="resume.education.master" components={[<b key="0" />]} />
               </Checkbox>
             </Frame>
           </Fieldset>
         </Tab>
-        <Tab title="Experience">
+
+        <Tab title={t('resume.tabs.experience')}>
           <Fieldset legend="" width="100%">
             <Frame padding="$10">
               <Checkbox readOnly checked>
-                <b>Immersive Technologies Developer</b> at CTIC Technological Center (Oct 2021 –
-                Present)
+                <Trans i18nKey="resume.experience.ctic" components={[<b key="0" />]} />
               </Checkbox>
               <Checkbox readOnly checked={false}>
-                <b>Web Developer</b> at Imascono (Jun 2021 – Oct 2021)
+                <Trans i18nKey="resume.experience.imascono" components={[<b key="0" />]} />
               </Checkbox>
               <Checkbox readOnly checked={false}>
-                <b>Mobile Developer</b> at GooApps (Nov 2019 – Oct 2020)
+                <Trans i18nKey="resume.experience.gooapps" components={[<b key="0" />]} />
               </Checkbox>
               <Checkbox readOnly checked={false}>
-                <b>Web Developer</b> at Adele Robots (Jan 2019 – May 2019)
+                <Trans i18nKey="resume.experience.adele" components={[<b key="0" />]} />
               </Checkbox>
             </Frame>
           </Fieldset>
         </Tab>
-        <Tab title="Skills">
-          <Fieldset legend="Programming" width="100%" height="170px" className="overflow-y-scroll">
+
+        <Tab title={t('resume.tabs.skills')}>
+          <Fieldset
+            legend={t('resume.skills.programming')}
+            width="100%"
+            height="170px"
+            className="overflow-y-scroll"
+          >
             <Frame padding="$10" className="flex flex-col gap-2">
               <div>
-                • Extended Realities (XR)
+                {t('resume.tech.xr')}
                 <ProgressBar percent={99} />
               </div>
-
               <div>
-                • Unity / C#
+                {t('resume.tech.unity')}
                 <ProgressBar percent={75} />
               </div>
-
               <div>
-                • React / Redux
+                {t('resume.tech.react')}
                 <ProgressBar percent={75} />
               </div>
-
               <div>
-                • ThreeJS / Javascript
+                {t('resume.tech.three')}
                 <ProgressBar percent={75} />
               </div>
-
               <div>
-                • HTML / CSS
+                {t('resume.tech.html')}
                 <ProgressBar percent={65} />
               </div>
-
               <div>
-                • Git
+                {t('resume.tech.git')}
                 <ProgressBar percent={65} />
               </div>
-
               <div>
-                • Blender
+                {t('resume.tech.blender')}
                 <ProgressBar percent={65} />
               </div>
-
               <div>
-                • Docker
+                {t('resume.tech.docker')}
                 <ProgressBar percent={35} />
               </div>
             </Frame>
           </Fieldset>
-          <Fieldset legend="Languages" width="100%">
+
+          <Fieldset legend={t('resume.skills.languages')} width="100%">
             <Frame padding="$10">
               <div>
-                • Spanish
+                {t('resume.tech.spanish')}
                 <ProgressBar percent={100} />
               </div>
-
               <div>
-                • English
+                {t('resume.tech.english')}
                 <ProgressBar percent={70} />
               </div>
             </Frame>
           </Fieldset>
         </Tab>
       </Tabs>
-      <Button className="mt-2 w-fit self-end">Download CV</Button>
+
+      <Button className="mt-2 w-fit self-end">{t('resume.skills.cv')}</Button>
     </div>
   );
 }
